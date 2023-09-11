9c4388aa0fe924e4438cc0bf146d9ad97583d670
rohanmane21 <143586966+rohanmane21@users.noreply.github.com>
2023-09-11 09:20:09 +0530

    Update create_instance.py

--- pipeline3/create_instance.py
+++ pipeline3/create_instance.py
@@ -1,10 +1,10 @@
 import boto3
 
 # Get environment variables
-# import os
-AWS_ACCESS_KEY_ID = 'AKIASYYM73UYQV2BP2S4'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_REGION = 'ap-south-1' 
+import os
+AWS_ACCESS_KEY_ID = os.environ.get('AKIASYYM73UYQV2BP2S4')
+AWS_SECRET_ACCESS_KEY = os.environ.get('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')
+AWS_REGION = os.environ.get('ap-south-1' )
 # Create an EC2 client
 ec2 = boto3.client('ec2', region_name=AWS_REGION, aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
