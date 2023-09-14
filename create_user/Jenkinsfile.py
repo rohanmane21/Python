867764220da6b5c51f9255383dc2a1c62a3e752a
rohanmane21 <143586966+rohanmane21@users.noreply.github.com>
2023-09-14 21:28:56 +0530

    Update Jenkinsfile.py

--- create_user/Jenkinsfile.py
+++ create_user/Jenkinsfile.py
@@ -2,11 +2,9 @@ import boto3
 import json
 
 user = boto3.client('iam')
-def create_policy():
-    with open('pipeline4/policy.json', 'r') as f:
-        policy_document = json.load(f)
-    response = user.create_policy(
-        PolicyName="All_Policies",
-        PolicyDocument=json.dumps(policy_document) 
+
+def create_user():
+    response = user.create_user(
+        UserName='boburao'
     )
-create_policy()
+create_user()
