7276709899eeb7a8eaa6643177d5a36fcc56867f
rohanmane21 <143586966+rohanmane21@users.noreply.github.com>
2023-09-14 13:58:42 +0530

    Update policy.py

--- pipeline4/policy.py
+++ pipeline4/policy.py
@@ -2,9 +2,17 @@ import boto3
 import json
 
 user = boto3.client('iam')
-with open('policy.json', 'r') as f:
-    policy_document = json.load(f)
-response = user.create_policy(
-    PolicyName="All_P",
-    PolicyDocument=json.dumps(policy_document) 
-)
+def create_policy():
+    with open('pipeline4/policy.json', 'r') as f:
+        policy_document = json.load(f)
+    response = user.create_policy(
+        PolicyName="All_Policy",
+        PolicyDocument=json.dumps(policy_document) 
+    )
+def create_user():
+    response = iam.create_user(
+        UserName='boburao'
+    )
+    print(response)
+
+create_policy()
