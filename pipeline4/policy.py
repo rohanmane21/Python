43f3501b837f8faeca2a67bbe550e86b89718f1f
rohanmane21 <143586966+rohanmane21@users.noreply.github.com>
2023-09-14 13:14:30 +0530

    Update policy.py

--- pipeline4/policy.py
+++ pipeline4/policy.py
@@ -3,8 +3,8 @@ import json
 user=boto3.client('iam')
 
 response = user.create_policy(
-PolicyName=policy_name,
-PolicyDocument=json.load(policy.json),
+# PolicyName=policy_name,
+PolicyDocument=json.load('policy.json'),
 Description='All policy'
 )
 
