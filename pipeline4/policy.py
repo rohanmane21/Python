2e1c27650d4a9249363ba5d2be5ce0a4ae4ec585
rohanmane21 <143586966+rohanmane21@users.noreply.github.com>
2023-09-14 15:54:10 +0530

    Update policy.py

--- pipeline4/policy.py
+++ pipeline4/policy.py
@@ -13,11 +13,11 @@ def create_user():
     response = user.create_user(
         UserName='boburao'
     )
-create_user()
+# create_user()
 def attach_policy():
     response = user.attach_user_policy(
     UserName= 'boburao',
     PolicyArn='arn:aws:iam::190616427825:policy/All_Policy'
 )
-attach_policy()
+# attach_policy()
 # create_policy()
